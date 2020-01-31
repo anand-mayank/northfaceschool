b964eabed232ee729a15e03ae6ba441cb35cb8eb
Mayank_Anand <35091448+anand-mayank@users.noreply.github.com>
2020-02-01 01:02:16 +0530

    Update project2.sql

--- project2.sql
+++ project2.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jan 04, 2018 at 04:28 PM
--- Server version: 5.7.14
--- PHP Version: 5.6.25
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
